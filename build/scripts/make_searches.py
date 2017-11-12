d374c20ba33c074e6a83e3b34c9cd2cbbe93674e
vorugantia <arjun.voruganti@gmail.com>
2017-11-12 17:43:53 -0500

    add make_searches.py script and generate query.json

---
+++ build/scripts/make_searches.py
@@ -0,0 +1,23 @@
+# Load senators_list.json and source_list.json.
+# With a template, make search terms by iterating through these two lists.
+# Write the array of search terms to output JSON file.
+import json
+
+with open('../input/senator_list.json') as senator_list:
+    senators = json.load(senator_list)
+
+with open('../input/source_list.json') as source_list:
+    sources = json.load(source_list)
+
+def get_template(source, senator):
+    return 'pub(%s) AND ("%s") AND stype.exact("Newspapers") AND pd(20170101-20171231)' % (source, senator)
+
+def make_searches():
+    searches = []
+    for source in sources:
+        for senator in senators:
+            searches.append(get_template(source, senator["name"]))
+    return {"searches": searches}
+
+with open('../output/query.json', 'w') as outfile:
+    json.dump(make_searches(), outfile)
